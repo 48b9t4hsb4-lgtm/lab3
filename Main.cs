f8ad30221ad2eb5e19976da4ba39e920ce9212a1
n3r3v1 <annakurochkina@icloud.com>
2026-03-23 18:33:18 +0200

    commit2_main_тестування

--- Main.cs
+++ Main.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Package p1 = new Package(1, 2.5);
+        Package p2 = new Package(2);
+
+        Console.WriteLine(p1);
+        Console.WriteLine(p2);
+
+        p1.ChangeStatus("В дорозі");
+        p2.ChangeStatus("Доставлено");
+
+        Console.WriteLine($"статус посилки 1: {p1.Status}");
+        Console.WriteLine($"статус посилки 2: {p2.Status}");
+
+        Console.WriteLine($"чи доставлена посилка 1: {p1.IsDelivered()}");
+        Console.WriteLine($"чи доставлена посилка 2: {p2.IsDelivered()}");
+
+        Console.WriteLine("Кількість посилок: " + Package.GetPackageCount());
+    }
+}
